6e7e9d4118b4e0128434aa5de7749986a2bd231b
BTKgamer <71685920+BTKcreations@users.noreply.github.com>
2023-12-08 11:02:33 +0530

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,30 +1,23 @@
 <?php
-$servername = "sql12.freesqldatabase.com"; // Replace with your server name
-$username = "sql12668423"; // Replace with your database username
-$password = "fX3CgScAWF"; // Replace with your database password
-$dbname = "sql12668423"; // Replace with your database name
+$servername = "sql12.freesqldatabase.com";
+$username = "sql12668423";
+$password = "fX3CgScAWF";
+$dbname = "sql12668423";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 
-// Retrieve form data
-$name = $_POST['name'];
-$mobile = $_POST['mobile'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-
-// Insert data into database
-$sql = "INSERT INTO users (name, mobile, email, password) VALUES ('$name', '$mobile', '$email', '$password')";
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+  echo "New record created successfully";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+  echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
